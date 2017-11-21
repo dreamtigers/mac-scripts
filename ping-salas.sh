cf88431e0939c05b7581f9bfaa979c503aeadca2
Ivan Gonzalez <ivcore@users.noreply.github.com>
2017-11-21 01:00:46 -0400

    Add some commets... for the future.

--- ping-salas.sh
+++ ping-salas.sh
@@ -1,60 +1,40 @@
 #!/bin/bash
 
 # Name of the classrooms, with their respective list of designated IPs.
+# To save the array in a variable, you have to enclose it between (), so bash
+# understands that you want to save the whole array, not only the first element.
 A=({2..26})
 E=({27..51})
 F=({52..76})
 ET=({77..100})
 classroom=
 
-if [[ $1 ]]; then
-  case ${1^^} in
-    A)
-      classroom="${1^^}[@]"
-      ;;
-    E)
-      classroom="${1^^}[@]"
-      ;;
-    F)
-      classroom="${1^^}[@]"
-      ;;
-    ET)
-      classroom="${1^^}[@]"
-      ;;
-    *)
-      echo 'Invalid.'
-      exit 1
-      ;;
-  esac
-else
-  PS3='Select a number and press Enter: '
-  option=('A' 'E' 'F' 'ET')
-  select room in "${option[@]}"
-  do
-    case "$room" in
-      A) 
-	classroom="${room}[@]"
-	break
-	;;
-      E) 
-	classroom="${room}[@]"
-	break
-	;;
-      F) 
-	classroom="${room}[@]"
-	break
-	;;
-      ET) 
-	classroom="${room}[@]"
-	break
-	;;
-      *) 
-	echo 'Invalid.'
-	;;
-    esac
-  done
-fi
+# ${n^^} turns the value of $n into caps.
+# Bash doesn't have pointers per se, but you can store in $classroom a string
+# which we will use later on. $m[@] means "all the elements of the array m".
+case ${1^^} in
+  A)
+    classroom="${1^^}[@]"
+    ;;
+  E)
+    classroom="${1^^}[@]"
+    ;;
+  F)
+    classroom="${1^^}[@]"
+    ;;
+  ET)
+    classroom="${1^^}[@]"
+    ;;
+  *)
+    echo 'Please use a valid option:'
+    echo 'A    E    F    ET'
+    exit 1
+    ;;
+esac
 
+# So we had a string with the name of our classroom + [@]. p.e. 'ET[@]'.
+# ${!n} takes the value of $n, uses it as a key, and returns the value of that
+# key. This is 'indirect expansion'. So ${!classroom} -> ${ET[@]} -> {77..100}.
 for i in "${!classroom}"; do
   if ping -c 1 "159.90.9.$i" > /dev/null; then
     echo "$i ON"
